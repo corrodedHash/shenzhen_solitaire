7ebc254294580294757eb26bd2466517dc46258e
Lukas Wölfer <lukas.woelfer@rwth-aachen.de>
2019-04-20 01:30:07 +0200

    Changed test, broke the iterators for test_chain

--- test/test_solver.py
+++ test/test_solver.py
@@ -3,7 +3,11 @@ from shenzhen_solitaire import solver
 
 from .boards import my_board
 
-A = solver.SolitaireSolver(my_board)
-B = A.solve()
-print(*B, sep='\n')
-print(len(B))
+def main() -> None:
+    A = solver.SolitaireSolver(my_board)
+    B = A.solve()
+    print(*B, sep='\n')
+    print(len(B))
+
+if __name__ == "__main__":
+    main()
